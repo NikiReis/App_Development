b4a786358a50224b7e2fcfa8d8be710cf55dd13f
NikiReis <linek.reis@iclinic.com.br>
2024-08-27 09:55:25 -0300

    construindo um counter

---
+++ projeto_01/script.js
@@ -0,0 +1,21 @@
+function imprimir(){
+    document.write("<p>Counter</p>");
+}
+
+// setInterval(imprimir, 1000);
+
+// setInterval(()=>{
+//     document.write("<p>Counter</p>");
+// },1000);
+
+// let counter = document.getElementById("contador");
+
+// var myTimer = setInterval(()=>{counter.innerHTML = "Hello </br>"},1000);
+// function stopTimer(){
+//     clearInterval(myTimer)
+// }
+
+setTimeout(imprimir,4000)
+setTimeout(()=>{
+
+},4000)
