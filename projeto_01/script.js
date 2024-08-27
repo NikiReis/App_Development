a252af2887accb0c2e0d239422c81d7543428ca1
NikiReis <linek.reis@iclinic.com.br>
2024-08-27 10:54:33 -0300

    novo entendimento sobre for

--- projeto_01/script.js
+++ projeto_01/script.js
@@ -4,12 +4,17 @@ let pessoa = {
     cargo: "CEO",
 };
 
-let usuarios = [
-    { nome: "Victor", idade: 23, cargo: "Dev" },
-    { nome: "Felipe", idade: 23, cargo: "Dev" },
-    { nome: "Linek Reis", idade: 22, cargo: "King"},
-];
+// let usuarios = [
+//     { nome: "Victor", idade: 23, cargo: "Dev" },
+//     { nome: "Felipe", idade: 23, cargo: "Dev" },
+//     { nome: "Linek Reis", idade: 22, cargo: "King"},
+// ];
 
-for (let i = 0; i < usuarios.length; i++) {
-    console.log(usuarios[i].nome);
-}
+// for (let i = 0; i < usuarios.length; i++) {
+//     console.log(usuarios[i].nome);
+// }
+
+let {nome, idade, cargo} = pessoa
+console.log(nome)
+console.log(idade)
+console.log(cargo)
